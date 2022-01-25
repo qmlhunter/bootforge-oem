01b43e730ed16ef4a93e8164088d80e80f911372
Quinntavich <46363605+QMLHUNTER@users.noreply.github.com>
2022-01-25 10:57:25 -0800

    Stable

--- One-click-OEM-unlock/Unlock(New Devices).bat
+++ One-click-OEM-unlock/Unlock(New Devices).bat
@@ -4,6 +4,9 @@ TITLE TO UNLOCK BOOTLOADER
 COLOR 12
 ECHO MAKE SURE YOU"RE IN FASTBOOT MODE AND YOU HAVE YOUR DEVICE DRIVERS INSTALLED CORRECTLY     
 ECHO(
+ECHO (If you get Waiting for any device error switch USB ports or make sure drivers are installed correctly)          
+ECHO(
+ECHO(
 ECHO(
 ECHO(
 ECHO(
