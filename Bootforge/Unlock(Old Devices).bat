01ab27cd427135c7d0edb36732e933abdb4d9bf9
Quinntavich <46363605+qmlhunter@users.noreply.github.com>
2022-04-11 08:53:23 -0700

    Update Unlock(Old Devices).bat

--- Bootforge/Unlock(Old Devices).bat
+++ Bootforge/Unlock(Old Devices).bat
@@ -19,5 +19,5 @@ fastboot oem unlock
 fastboot format userdata
 fastboot reboot
 ECHO DONE
-if %errorlevel%==131 ECHO Failed! Remember to double check Fastboot drivers and that your phone is prepped properly!
+if %errorlevel%==0 ECHO Failed! Remember to double check Fastboot drivers and that your phone is prepped properly!
 SET /P ANSWER=ALL DONE PRESS ENTER TO EXIT
